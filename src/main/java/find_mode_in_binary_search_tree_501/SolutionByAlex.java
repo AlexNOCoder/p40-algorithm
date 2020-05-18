f8952643b4663f34db708d65d8d378b9bb560218
1140788816@qq.com <lyt7320662!>
2020-05-18 23:13:57 +0800

    find mode in binary search tree

--- src/main/java/find_mode_in_binary_search_tree_501/SolutionByAlex.java
+++ src/main/java/find_mode_in_binary_search_tree_501/SolutionByAlex.java
@@ -7,23 +7,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
+/**
+ *时间复杂度，空间复杂度爆炸，
+ */
 public class SolutionByAlex {
 
-    public static void main(String[] args) {
-        TreeNode root = new TreeNode(4);
-        root.left =  new TreeNode(4);
-        root.right = new TreeNode(4);
-        root.right.right = new TreeNode(4);
-        root.right.right.right = new TreeNode(4);
-//        inOrder(root);
-//        findMode(root);
-        System.out.println(findEqualNum(root));
-    }
-
     public static int[] findMode(TreeNode root) {
-        int[] orderResult = inOrder(root);
-        int[]  result = new int[4];
+        List<Integer> orderResult = inOrder(root);
         HashMap<Integer,Integer> numMap = new HashMap<>();
         int maxNum = 0;
         for(Integer i:orderResult){
@@ -41,57 +31,27 @@ public class SolutionByAlex {
                 }
             }
         }
-        int i =0;
+        List<Integer> result = new ArrayList<>();
         for(Map.Entry en:numMap.entrySet()){
             if(en.getValue().equals(maxNum)){
-               Integer value = (Integer) en.getKey();
-               result[i] = value.intValue();
-               i++;
+               result.add((Integer) en.getKey());
             }
         }
-
-        return result;
+        int[] count = new int[result.size()];
+        for(int i=0;i<result.size();i++){
+            count[i] = result.get(i);
+        }
+        return count;
     }
-//    private static  int count =0;
-    private  static int[] result =new int[5];
-    public static int[] inOrder(TreeNode root){
-
+    private  static List<Integer> result = new ArrayList<>();
+    public static List<Integer> inOrder(TreeNode root){
         if(root != null){
             inOrder(root.left);
             System.out.println(root.val);
-            result[count] = root.val;
-            count++;
+            result.add(root.val);
             inOrder(root.right);
         }
         return result;
     }
 
-    public static void preOrder(TreeNode root){
-        if(root != null){
-            preOrder(root.left);
-            preOrder(root.right);
-        }
-    }
-    private static int count = 1;
-    public static  int  findEqualNum(TreeNode root){
-
-        int val = root.val;
-        if(root.left != null){
-            int val_left = root.left.val;
-            if(val == val_left){
-                count++;
-                findEqualNum(root.left);
-            }
-        }
-        if(root.right != null){
-            int val_right = root.right.val;
-            if(val == val_right){
-                count++;
-                findEqualNum(root.left);
-            }
-        }
-
-        return count;
-
-    }
 }
