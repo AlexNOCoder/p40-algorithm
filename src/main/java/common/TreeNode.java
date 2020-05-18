7220b9396f4dc4be5e1f9edc76ad967890b86c62
luoyuntian <1140788816@qq.com>
2020-05-18 15:49:46 +0800

    update TreeNode

--- src/main/java/common/TreeNode.java
+++ src/main/java/common/TreeNode.java
@@ -1,7 +1,7 @@
 package common;
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x){val = x;}
+   public int val;
+   public TreeNode left;
+   public TreeNode right;
+   public TreeNode(int x){val = x;}
 }
