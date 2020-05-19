5bbdf3a90af2aec5d1d0cabf26382e99d1aeca16
lengpp <850900362@qq.com>
2020-05-19 10:50:44 +0800

    除法博弈，动态

--- src/main/java/divisor_game_1025/SolutionByLp.java
+++ src/main/java/divisor_game_1025/SolutionByLp.java
@@ -5,6 +5,11 @@ package divisor_game_1025;
  * @DATE: 2020/5/19
  **/
 public class SolutionByLp {
+    /**
+     * 动态
+     * @param N
+     * @return
+     */
     public boolean divisorGame(int N) {
         if(N==1){
             return false;
@@ -19,5 +24,14 @@ public class SolutionByLp {
             s[i] = !s[i-1];
         }
         return s[N-1];
+    }
+
+    /**
+     * 归纳法牛逼
+     * @param N
+     * @return
+     */
+    public boolean divisorGame2(int N) {
+        return N%2==0;
     }
 }
