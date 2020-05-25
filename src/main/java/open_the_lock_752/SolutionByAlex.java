a69f12609e5def9871ec0f35e85a31c5212046da
luoyuntian <1140788816@qq.com>
2020-05-25 12:33:40 +0800

    update open the lock

--- src/main/java/open_the_lock_752/SolutionByAlex.java
+++ src/main/java/open_the_lock_752/SolutionByAlex.java
@@ -1,7 +1,6 @@
 package open_the_lock_752;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class SolutionByAlex {
     public static void main(String[] args) {
@@ -12,22 +11,48 @@ public class SolutionByAlex {
     }
 
 
-    //每一个状态的前面会有八种情况
+    // 某一个状态的上一种情况有八种,每一位都可以+1和-1
     public static int openLock(String[] deadends, String target) {
-        List<String> dead = Arrays.asList(deadends);
-        if(dead.contains("0000")){
+        List<String> deads =  Arrays.asList(deadends);
+        HashSet<String> hashSet = new HashSet<>(deads);
+        if(deads.contains("0000")){
             return -1;
         }
-        char[] str = target.toCharArray();
-        int count =0;
-        for(int i=0;i<str.length;i++){
+        Queue<String> queue = new LinkedList<>();
+        queue.add("0000");
+        int count = 0;
+        while(!queue.isEmpty()){
+            int size =  queue.size();
+            while(size -- >0){
+                String temp = queue.poll();
+                if(target.equals(temp)){
+                    return count;
+                }
+                char[] chars = temp.toCharArray();
+                for(int index = 0;index <4 ;index++){
+                    int i = chars[index] - '0';
+                    //-1
+                    chars[index] = (char) ('0' +(i + 9)%10);
+                    String s = new String(chars);
+                    if(!hashSet.contains(s)){
+                        queue.offer(s);
+                        hashSet.add(s);
+                    }
+                    //+1
+                    chars[index] = (char) ('0' +(i + 11)%10);
+                    s = new String(chars);
+                    if(!hashSet.contains(s)){
+                        queue.offer(s);
+                        hashSet.add(s);
+                    }
+                    //还原
+                    chars[index] = (char) ('0' +i);
+                }
+            }
+            count ++ ;
+        }
 
-        }
-        if (count > -1){
-            return count;
-        }else {
-            return -1;
-        }
+        return -1;
     }
 
 
