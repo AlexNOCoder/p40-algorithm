f1c010ae0677910c329a12ff5b3b40d6298bff8a
luoyuntian <1140788816@qq.com>
2020-06-04 17:02:00 +0800

    update new_21_game

--- src/main/java/new_21_game_837/SolutionByAlex.java
+++ src/main/java/new_21_game_837/SolutionByAlex.java
@@ -3,21 +3,50 @@ package new_21_game_837;
 
 public class SolutionByAlex {
 
-    public double new21Game(int N, int K, int W) {
+    public static void main(String[] args) {
+        System.out.println(new21Game(21,17,10));
+    }
+
+
+    public static double new21Game(int N, int K, int W) {
         double[] sum = new double[K+W];
+        double[] p = new double[K+W];
         if(K ==0){
             return 1.0;
         }
-        //分段函数
-        for(int i=0;i<K+W;i++){
+        sum[0] = 1;
+        p[0] = 1;
+        //分段函数 以 N=21；K=17；W=10为例
+        for(int i=1;i<K+W;i++){
             //i <= k && i <= w : p[i] = sum[i-1]/w
+            //eg. i=7
+            if(i <=K && i<= W){
+                p[i] = sum[i-1]/W;
+
+            }
 
             //i <= k && i > w : p[i] = (sum[i-1] - sum[i-w-1])/w
+            //eg. i = 13
+            if(i <=K && i > W){
+                p[i] = (sum[i-1] - sum[i-W-1])/W;
 
-            //i > k && i <= w : p[i] = sum[K-1]/w
+            }
+
+            //i > k && i <= w : p[i] = (sum[i-1] - sum[i-w-1])/w
+            //eg. i = 13
+            if(i > K && i <= W){
+                p[i] = sum[K-1]/W;
+
+            }
 
             //i > k && i > w : p[i] = (sum[K-1] - sum[i-W-1])/w
+            //eg. i = 20
+            if(i > K && i > W){
+                p[i] = (sum[K-1] - sum[i-W-1])/W;
+            }
+            sum[i]= sum[i-1]+p[i];
+
          }
-        return 0.0;
+        return (sum[N] - sum[K-1])/(sum[K+W-1] - sum[K-1]);
     }
 }
