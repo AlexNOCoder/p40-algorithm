86bf653ee30e8df3956106d40833408393b9427a
1140788816@qq.com <lyt7320662!>
2020-06-04 23:58:31 +0800

    add keys_and_rooms

---
+++ src/main/java/keys_and_rooms_841/SolutionByAlex.java
@@ -0,0 +1,12 @@
+package keys_and_rooms_841;
+
+import java.util.List;
+
+public class SolutionByAlex {
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        return false;
+    }
+}
+
+
