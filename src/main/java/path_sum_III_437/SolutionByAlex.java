0a203b72dcc3fea6b7046071f170c5d365c8fa48
luoyuntian <1140788816@qq.com>
2020-06-29 14:12:47 +0800

    udate path_sum

--- src/main/java/path_sum_III_437/SolutionByAlex.java
+++ src/main/java/path_sum_III_437/SolutionByAlex.java
@@ -3,15 +3,30 @@ package path_sum_III_437;
 
 import common.TreeNode;
 
+//双层递归，第一层遍历所有节点，第二层遍历该节点的所有使条件成立的可能
 public class SolutionByAlex {
 
+    private int count=0;
     public int pathSum(TreeNode root, int sum) {
-
-
-        return 0;
+        if(root == null){
+            return 0;
+        }
+        getPathSumNumber(root,sum);
+        pathSum(root.left,sum);
+        pathSum(root.right,sum);
+        return count;
     }
 
-    public int getPathSumNumber(TreeNode root,int sum){
-        return 1;
+    public void getPathSumNumber(TreeNode root,int sum){
+        if(root == null){
+            return;
+        }
+        sum -= root.val;
+        if(sum == 0){
+            count++;
+        }
+        getPathSumNumber(root.left,sum);
+        getPathSumNumber(root.right,sum);
+
     }
 }
