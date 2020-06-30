8a60a32ef09b3f1ecc05aa19e45301d79efb5afc
luoyuntian <1140788816@qq.com>
2020-06-30 17:42:14 +0800

    add minimum_depth_of_binary_tree

---
+++ src/main/java/minimum_depth_of_binary_tree_111/SolutionByAlex.java
@@ -0,0 +1,42 @@
+package minimum_depth_of_binary_tree_111;
+
+import common.TreeNode;
+
+import java.util.*;
+
+public class SolutionByAlex {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+
+        System.out.println(minDepth(root));
+    }
+
+    public static int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.right == null || root.left == null){
+            return 1;
+        }
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(root);
+        int path = 0;
+        while(!queue.isEmpty()){
+            path++;
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.poll();
+                if(node.left == null && node.right == null){
+                    return path;
+                }
+                queue.offer(node);
+            }
+        }
+        return path;
+    }
+
+}
