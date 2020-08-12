f15db1d53686e95a2a407c7cb8a6dac189788cb1
luoyuntian <1140788816@qq.com>
2020-08-12 08:03:56 +0800

    add huawei HJ9

--- src/main/java/huawei_exercise_total_108/HJ9.java
+++ src/main/java/huawei_exercise_total_108/HJ9.java
@@ -1,12 +1,24 @@
 package huawei_exercise_total_108;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 public class HJ9 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String input = sc.next();
+            char[] inputChars = input.toCharArray();
+            Set<Integer> set = new HashSet<>();
+            for(int i=inputChars.length-1;i>=0;i--){
+                if(!set.contains(Integer.valueOf(inputChars[i]))){
+                    set.add(Integer.valueOf(inputChars[i]));
+                    System.out.print(inputChars[i]);
+                }
+            }
 
+        }
     }
 }
