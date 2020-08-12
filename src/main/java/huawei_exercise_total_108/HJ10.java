ae486c1c3d314983875450ffe87f72691f176945
luoyuntian <1140788816@qq.com>
2020-08-12 08:54:02 +0800

    add huawei HJ10

---
+++ src/main/java/huawei_exercise_total_108/HJ10.java
@@ -0,0 +1,26 @@
+package huawei_exercise_total_108;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HJ10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String input = sc.next();
+            char[] inputChars = input.toCharArray();
+            Set<Integer> set = new HashSet<>();
+            int count =0;
+            for(int i=inputChars.length-1;i>=0;i--){
+                if(!set.contains(Integer.valueOf(inputChars[i]))){
+                    set.add(Integer.valueOf(inputChars[i]));
+                    count++;
+                }
+            }
+            System.out.println(count);
+
+        }
+    }
+}
