fadf080d55093f3c595dbaf9eb1f47e37c61efc4
luoyuntian <1140788816@qq.com>
2020-08-23 13:17:34 +0800

    update valid_parentheses_20

--- src/main/java/valid_parentheses_20/SolutionByAlex.java
+++ src/main/java/valid_parentheses_20/SolutionByAlex.java
@@ -46,4 +46,48 @@ public class SolutionByAlex {
 
     }
 
+
+    //解法2，思路相同，代码看起来漂亮点
+    public static boolean isValid2(String input){
+        if(input.length() == 0){
+            return true;
+        }
+        char[] inputChars = input.toCharArray();
+        Stack<Character> stack  = new Stack<>();
+        stack.push(inputChars[0]);
+        int index=1;
+        while(!stack.isEmpty() && index < inputChars.length){
+            Character stackTop  = stack.peek();
+            if(isMatch(stackTop,inputChars[index])){
+                stack.pop();
+                index++;
+                if(stack.isEmpty() && index < inputChars.length){
+                    stack.push(inputChars[index]);
+                    index++;
+                }
+            }else {
+                stack.push(inputChars[index]);
+                index++;
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static boolean isMatch(char c1,char c2){
+        if(c1 == '(' && c2 == ')'){
+            return true;
+        }else  if(c1 == '{' && c2 == '}'){
+            return true;
+        }else if(c1 =='['&& c2==']' ){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
 }
