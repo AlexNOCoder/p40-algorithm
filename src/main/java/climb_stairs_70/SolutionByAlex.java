a4bb53407192ef5a9f2ff5f77a2409e403e2c5e4
luoyuntian <1140788816@qq.com>
2020-08-30 11:51:01 +0800

    add climb_stairs_70 sloutionByAlex

---
+++ src/main/java/climb_stairs_70/SolutionByAlex.java
@@ -0,0 +1,24 @@
+package climb_stairs_70;
+
+public class SolutionByAlex {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(7));
+    }
+
+    public static int climbStairs(int n) {
+        if(n ==1){
+            return 1;
+        }
+        if(n ==2){
+            return 2;
+        }
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+}
