79e413917681fdcceac4366a6a22f6ffe6ee4e61
luoyuntian <1140788816@qq.com>
2020-11-03 15:17:36 +0800

    update implement_strStr_28

--- src/main/java/implement_strStr_28/SolutionByAlex.java
+++ src/main/java/implement_strStr_28/SolutionByAlex.java
@@ -1,10 +1,13 @@
 package implement_strStr_28;
 
-
+/**
+   * @date 2020-11-03 15:15
+   * @description 此处可以使用KMP算法，后续补上，暂时刷分用
+   */
 public class SolutionByAlex {
     public int strStr(String haystack, String needle) {
 
-        return 0;
+        return haystack.indexOf(needle);
     }
 
 }
