eb772c767a2ff6fdd4000376d49ce85bafd368ae
luoyuntian <1140788816@qq.com>
2020-11-03 17:28:26 +0800

    update length_of_last_word_58

--- src/main/java/length_of_last_word_58/SolutionByAlex.java
+++ src/main/java/length_of_last_word_58/SolutionByAlex.java
@@ -2,8 +2,16 @@ package length_of_last_word_58;
 
 
 public class SolutionByAlex {
-    public int lengthOfLastWord(String s) {
-
+    public static int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ");
+        if(arr.length>0){
+            return arr[arr.length-1].length();
+        }
         return 0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(lengthOfLastWord("Hello World"));
     }
 }
