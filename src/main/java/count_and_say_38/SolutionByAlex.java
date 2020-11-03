4e52f1b2f01a322da17615c83e25fc778290d168
luoyuntian <1140788816@qq.com>
2020-11-03 15:31:07 +0800

    create count_and_say_38

--- src/main/java/count_and_say_38/SolutionByAlex.java
+++ src/main/java/count_and_say_38/SolutionByAlex.java
@@ -4,9 +4,15 @@ public class SolutionByAlex {
 
     public String countAndSay(int n) {
 
+
         return null;
     }
 
+    //对前一个数字进行描述
+    public String sayPreNumber(String preNumber){
+
+        return null;
+    }
 }
 
 
