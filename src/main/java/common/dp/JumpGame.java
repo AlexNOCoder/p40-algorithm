093739e8eace8f87b4a7a46a5301dd7aa3e2c2a3
luoyuntian <1140788816@qq.com>
2020-11-08 14:15:33 +0800

    add JumpGame

---
+++ src/main/java/common/dp/JumpGame.java
@@ -0,0 +1,29 @@
+package common.dp;
+public class JumpGame {
+    public static boolean jumpGame(int[] A){
+        int n = A.length;
+        //0到n-1块石头
+        boolean[] f = new boolean[n];
+        //初始化
+        f[0] = true;
+
+        //从前往后遍历
+        for(int i=1; i<n;++i){
+            //先设置为不可达
+            f[i] = false;
+            //前一个石头 j
+            //最后一条是从 j 到 i
+            for(int j=0;j < i;++j){
+                //条件： j可达，并且j可以跳到i
+                if(f[j] && j + A[j] >= i){
+                    //状态转移
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        //结果为f[n-1]
+        return f[n -1];
+    }
+}
