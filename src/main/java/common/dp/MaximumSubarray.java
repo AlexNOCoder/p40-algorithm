7df2ad205d27e003ab3444649471275a830d97a3
luoyuntian <1140788816@qq.com>
2020-11-17 16:34:10 +0800

    update MaximumSubarray

--- src/main/java/common/dp/MaximumSubarray.java
+++ src/main/java/common/dp/MaximumSubarray.java
@@ -8,14 +8,21 @@ public class MaximumSubarray {
         //如果前一位的最大子序列之和大于0了，就把前面的舍去
         //dp[i] = nums[i] + Math.max(dp[i-1],0)
         int n= nums.length;
+        //开辟数组
         int[] f = new int[n];
+
+        //初始化
         f[0] = nums[0];
+        //初始化最大值
         int max = f[0];
+
+        //从小到大遍历
         for(int i=1;i<n;i++){
+            //状态转移方程
             f[i] = nums[i] + Math.max(f[i-1],0);
-            if(max < f[i]){
-                max = f[i];
-            }
+
+            //更新最大值
+            max = Math.max(max,f[i]);
         }
         return max;
     }
