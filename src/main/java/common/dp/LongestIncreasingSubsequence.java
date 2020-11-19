c3918a3fc18ad46da63cd06cac0378eab68d888f
luoyuntian <1140788816@qq.com>
2020-11-19 14:34:32 +0800

    update LIS

--- src/main/java/common/dp/LongestIncreasingSubsequence.java
+++ src/main/java/common/dp/LongestIncreasingSubsequence.java
@@ -8,21 +8,21 @@ public class LongestIncreasingSubsequence {
             return 0;
         }
         //开辟数组
-        int[] dp = new int[n];
+        int[] f = new int[n];
         //初始化
-        dp[0] = 1;
+        f[0] = 1;
         //初始化结果，最大值为dp[0]
-        int max =dp[0];
+        int max =f[0];
         //从小到大遍历
         for(int i=1 ;i<nums.length; i++){
             //初始化最大值
-            dp[i] =1;
+            f[i] =1;
             for(int j=0 ; j<i ;j++){
                 if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
+                    f[i] = Math.max(f[i],f[j]+1);
                 }
             }
-            max = Math.max(max,dp[i]);
+            max = Math.max(max,f[i]);
         }
         return max;
     }
