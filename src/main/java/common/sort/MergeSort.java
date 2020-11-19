c10a6973dc540043b6052eae1035bb9786cb8c47
luoyuntian <1140788816@qq.com>
2020-11-19 08:46:26 +0800

    update mergesort

--- src/main/java/common/sort/MergeSort.java
+++ src/main/java/common/sort/MergeSort.java
@@ -1,32 +1,53 @@
 package common.sort;
 
-import java.util.Arrays;
 
 public class MergeSort {
 
-    public static int[] mergeSort(int[] array){
-        if(array.length<2){
-            return array;
+
+    public static void main(String[] args) {
+        int[] arr = {11,44,23,67,88,65,34,48,9,12};
+        int[] tmp = new int[arr.length];    //新建一个临时数组存放
+        mergeSort(arr,0,arr.length-1,tmp);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
-        int mid = array.length/2;
-        int[] left = Arrays.copyOfRange(array,0,mid);
-        int[] right = Arrays.copyOfRange(array,mid,array.length);
-        return merge(mergeSort(left),mergeSort(right));
     }
 
-    public static int[] merge(int[] left,int[] right){
-        int[] result = new int[left.length+right.length];
-        for(int index=0,i = 0,j=0;index<result.length;index++){
-            if(i >= left.length){
-                result[index] = right[j++];
-            }else if(j >right.length){
-                result[index] = left[i++];
-            }else if(left[i] > right[j]){
-                result[index] = right[j++];
-            }else {
-                result[index] = left[i++];
+    public static void merge(int[] arr,int low,int mid,int high,int[] tmp){
+        int i = 0;
+        //左边序列和右边序列起始索引
+        int j = low,k = mid+1;
+        while(j <= mid && k <= high){
+            if(arr[j] < arr[k]){
+                tmp[i++] = arr[j++];
+            }else{
+                tmp[i++] = arr[k++];
             }
         }
-        return result;
+        //若左边序列还有剩余，则将其全部拷贝进tmp[]中
+        while(j <= mid){
+            tmp[i++] = arr[j++];
+        }
+
+        while(k <= high){
+            tmp[i++] = arr[k++];
+        }
+
+        for(int t=0;t<i;t++){
+            arr[low+t] = tmp[t];
+        }
     }
+
+    public static void mergeSort(int[] arr,int low,int high,int[] tmp){
+        if(low<high){
+            int mid = (low+high)/2;
+            //对左边序列进行归并排序
+            mergeSort(arr,low,mid,tmp);
+            //对右边序列进行归并排序
+            mergeSort(arr,mid+1,high,tmp);
+            //合并两个有序序列
+            merge(arr,low,mid,high,tmp);
+        }
+    }
+
 }
