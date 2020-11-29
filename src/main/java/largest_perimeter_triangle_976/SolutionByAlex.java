e93fbef98cdf30c5fd2ec66adc594753b127fa52
luoyuntian <1140788816@qq.com>
2020-11-29 17:18:29 +0800

    update largest_perimeter_triangle_976

--- src/main/java/largest_perimeter_triangle_976/SolutionByAlex.java
+++ src/main/java/largest_perimeter_triangle_976/SolutionByAlex.java
@@ -1,8 +1,35 @@
 package largest_perimeter_triangle_976;
 
 
+import java.util.Arrays;
+
 public class SolutionByAlex {
-    public int largestPerimeter(int[] A) {
-        return 0;
+    public static int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        int size = A.length;
+        if(size <3 ){
+            return 0;
+        }
+        //最大值肯定是三条边长度都是最大的，
+        int max=0;
+        for(int i=0;i+2<size;i++){
+            if(A[i+2] - A[i+1] < A[i]){
+                //有一组满足可以拼出三角形,查看是否更新max
+                max = Math.max(max,A[i+2]+A[i+1]+A[i]);
+            }
+        }
+        return max;
     }
+
+    public static void main(String[] args) {
+        int[] test1 ={2,1,2};
+        int[] test2 ={1,2,1};
+        int[] test3 ={3,2,3,4};
+        int[] test4 ={3,6,2,3};
+        System.out.println(largestPerimeter(test1));
+        System.out.println(largestPerimeter(test2));
+        System.out.println(largestPerimeter(test3));
+        System.out.println(largestPerimeter(test4));
+    }
+
 }
