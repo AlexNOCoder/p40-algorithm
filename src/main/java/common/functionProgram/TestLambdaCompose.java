685855aa85cb2223a7ba105a10d233c4fba454a1
luoyuntian <1140788816@qq.com>
2020-12-19 15:30:59 +0800

    update TestLambdaCompose

--- src/main/java/common/functionProgram/TestLambdaCompose.java
+++ src/main/java/common/functionProgram/TestLambdaCompose.java
@@ -5,14 +5,14 @@ public class TestLambdaCompose {
     }
 
     public static void main(String[] args) {
-        TestCompose.Function<Integer,Integer> triple = x -> x * 3;
+        Function<Integer,Integer> triple = x -> x * 3;
 
-        TestCompose.Function<Integer,Integer> square = x -> x * x;
+        Function<Integer,Integer> square = x -> x * x;
 
         System.out.println(compose(triple,square).apply(3));
     }
 
-    public static TestCompose.Function<Integer,Integer> compose(final TestCompose.Function<Integer,Integer> f1, final TestCompose.Function<Integer,Integer> f2){
+    public static Function<Integer,Integer> compose(final Function<Integer,Integer> f1, final Function<Integer,Integer> f2){
         return x -> f1.apply(f2.apply(x));
     }
 }
