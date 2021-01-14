9d3b314d47859c96eea42ff95609f23168001a84
luoyuntian <1140788816@qq.com>
2021-01-14 09:55:48 +0800

    update EnumSingleton

--- src/main/java/common/designPattern/singleton/EnumSingleton.java
+++ src/main/java/common/designPattern/singleton/EnumSingleton.java
@@ -1,3 +1,15 @@
 package common.designPattern.singleton;
-public class EnumSingleton {
+public enum  EnumSingleton {
+    INSTANCE;
+    private Object data;
+    public Object getData(){
+        return data;
+    }
+
+    public void setData(Object data){
+        this.data = data;
+    }
+    public static EnumSingleton getInstance(){
+        return INSTANCE;
+    }
 }
