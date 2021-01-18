d9c7676a6de3255b8031e020d2bc1eb5a66dd75c
luoyuntian <1140788816@qq.com>
2021-01-18 15:49:07 +0800

    create startegy

--- src/main/java/common/designPattern/delegate/EmployeeA.java
+++ src/main/java/common/designPattern/delegate/EmployeeA.java
@@ -3,6 +3,6 @@ public class EmployeeA implements IEmployee{
 
     @Override
     public void doing(String command) {
-        System.out.println("我是员工A，我现在开始干： "+command+"工作");
+        System.out.println("我是员工A，我现在开始干"+command+"工作");
     }
 }
