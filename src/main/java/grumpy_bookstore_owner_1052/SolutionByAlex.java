795741c883cec429c6420e489aa377932e48aa57
luoyuntian <1140788816@qq.com>
2021-02-23 09:47:30 +0800

    create grumpy_bookstore_owner_1052

---
+++ src/main/java/grumpy_bookstore_owner_1052/SolutionByAlex.java
@@ -0,0 +1,10 @@
+package grumpy_bookstore_owner_1052;
+
+
+public class SolutionByAlex {
+
+    public int maxSatisfied(int[] customers, int[] grumpy, int X) {
+        //todo
+        return 0;
+    }
+}
