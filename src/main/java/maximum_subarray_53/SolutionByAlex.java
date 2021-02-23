12baaf7ef1c57ee3b56c31d16fa620a6659fd077
luoyuntian <1140788816@qq.com>
2021-02-23 10:09:09 +0800

    update grumpy_bookstore_owner_1052

--- src/main/java/maximum_subarray_53/SolutionByAlex.java
+++ src/main/java/maximum_subarray_53/SolutionByAlex.java
@@ -6,7 +6,7 @@ public class SolutionByAlex {
         //最大连续子数组
         //子数组是连续的
         //对于dp[i]表示[0...1]最大的子序列的值
-        //如果前一位的最大子序列之和大于0了，就把前面的舍去
+        //如果前一位的最大子序列之和小于0了，就把前面的舍去
         //dp[i] = nums[i] + Math.max(dp[i-1],0)
         int n= nums.length;
         int[] dp = new int[n];
