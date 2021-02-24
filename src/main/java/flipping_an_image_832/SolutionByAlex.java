4fb7be893b8cc5758ad3aad41cc5ff2fc01d3209
luoyuntian <1140788816@qq.com>
2021-02-24 13:50:20 +0800

    update flipping_an_image_832

--- src/main/java/flipping_an_image_832/SolutionByAlex.java
+++ src/main/java/flipping_an_image_832/SolutionByAlex.java
@@ -7,14 +7,15 @@ public class SolutionByAlex {
 
     public static int[][] flipAndInvertImage(int[][] A) {
         int m = A.length;
-        int n = A[0].length;
-
-
-
-        return null;
+        for(int i=0;i<m;i++){
+            int[] temp = A[i];
+            revert(temp,0,temp.length-1);
+            overturn(temp);
+        }
+        return A;
     }
 
-    public static int[] revert(int[] A,int left,int right){
+    public static void revert(int[] A,int left,int right){
         while(left < right){
             int temp = A[left];
             A[left] = A[right];
@@ -23,33 +24,38 @@ public class SolutionByAlex {
             right --;
 
         }
-        System.out.println(Arrays.toString(A));
-        return A;
     }
 
-    public static int[] overturn(int[] A){
+    public static void overturn(int[] A){
         for(int i=0;i<A.length;i++){
             A[i] = A[i] == 0 ? 1 : 0;
         }
-        return A;
     }
 
     public static void main(String[] args) {
-        int[] A = {1,2,3,4,5,6,7,8};
+//        int[] A = {1,2,3,4,5,6,7,8};
+//
+//
+//        int mid = A.length /2;
+//        System.out.println(Arrays.toString(A));
+//
+//        revert(A,0,A.length-1);
+//
+//        revert(A,0,mid);
+//
+//        revert(A,mid+1,A.length-1);
+//
+//
+//        int[] B = {1,0,1,0,1};
+//        overturn(B);
+//        System.out.println(Arrays.toString(B));
+        int[][] A = {{1,1,0},{1,0,1},{0,0,0}};
 
+        flipAndInvertImage(A);
+        System.out.println("=============");
+        for(int[] a1:A){
+            System.out.println(Arrays.toString(a1));
+        }
 
-        int mid = A.length /2;
-        System.out.println(Arrays.toString(A));
-
-        revert(A,0,A.length-1);
-
-        revert(A,0,mid);
-
-        revert(A,mid+1,A.length-1);
-
-
-        int[] B = {1,0,1,0,1};
-        overturn(B);
-        System.out.println(Arrays.toString(B));
     }
 }
