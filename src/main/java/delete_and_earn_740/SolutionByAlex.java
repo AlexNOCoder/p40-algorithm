668ab0b34063c1c7378d874bd466741f3d39ddba
luoyuntian <1140788816@qq.com>
2021-02-26 10:28:24 +0800

    create number_of_valid_words_for_each_puzzle_1178

--- src/main/java/delete_and_earn_740/SolutionByAlex.java
+++ src/main/java/delete_and_earn_740/SolutionByAlex.java
@@ -1,8 +1,19 @@
 package delete_and_earn_740;
 
+import java.util.Arrays;
 
+/*
+    1.选中任意一个数，删除它，并或得它的点数
+    2.删除每个比它小1和大1的数
+    3.循环
+ */
 public class SolutionByAlex {
     public int deleteAndEarn(int[] nums) {
+        //排序
+        Arrays.sort(nums);
+        //todo
+        //如果选了一个，它相邻的两个数就不能选
+
 
         return 0;
     }
---
+++ src/main/java/number_of_valid_words_for_each_puzzle_1178/SolutionByAlex.java
@@ -0,0 +1,10 @@
+package number_of_valid_words_for_each_puzzle_1178;
+
+import java.util.List;
+
+public class SolutionByAlex {
+    public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
+        //todo
+        return null;
+    }
+}
