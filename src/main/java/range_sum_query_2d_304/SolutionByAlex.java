c33d8e4e4c3c8c80d6d24fef97be4a466bed41d3
luoyuntian <1140788816@qq.com>
2021-03-02 10:01:21 +0800

    update range_sum_query_2d_304

--- src/main/java/range_sum_query_2d_304/SolutionByAlex.java
+++ src/main/java/range_sum_query_2d_304/SolutionByAlex.java
@@ -3,4 +3,12 @@ package range_sum_query_2d_304;
 
 public class SolutionByAlex {
 
+    public SolutionByAlex(int[][] matrix ){
+
+    }
+
+    public int sumRegion(int row1,int col1,int row2,int col2){
+
+        return 0;
+    }
 }
