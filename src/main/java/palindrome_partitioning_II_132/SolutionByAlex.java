adbd03911f640d6905c9e5cd52bd148298321248
luoyuntian <1140788816@qq.com>
2021-03-08 15:49:00 +0800

    create palindrome_partitioning_II_132

---
+++ src/main/java/palindrome_partitioning_II_132/SolutionByAlex.java
@@ -0,0 +1,66 @@
+package palindrome_partitioning_II_132;
+
+import java.util.*;
+
+public class SolutionByAlex {
+
+    public static int minCut(String s) {
+        List<List<String>> partition = partition(s);
+        if(partition == null && partition.size() == 0 ){
+            return 0;
+        }
+        int min = Integer.MAX_VALUE;
+        for(List<String> list :partition){
+            min = Math.min(min,list.size()-1);
+        }
+        return min;
+    }
+
+
+
+    public static List<List<String>> partition(String s) {
+        int len = s.length();
+        List<List<String>> res = new LinkedList<>();
+        if(len == 0){
+            return res;
+        }
+        //stack 保存回溯路径
+        Deque<String> stack = new ArrayDeque<>();
+        char[] charArray = s.toCharArray();
+        dfs(charArray,0,len,stack,res);
+
+        return res;
+
+    }
+    //深度优先遍历找出每一条可能的路径
+    public static void dfs(char[] charArray, int index, int len, Deque<String> path,List<List<String>> res){
+        //如果到达尾部则将这个结果加入到res中
+        if(index == len){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        //从index到len区间内判断
+        for (int i = index;i < len;i++){
+            if(!checkPalindrome(charArray,index,i)){
+                continue;
+            }
+            //深度优先遍历递归实现，找出每一种可能并加入到res中
+            path.addLast(new String(charArray,index,i+1-index));
+            dfs(charArray,i+1,len,path,res);
+            path.removeLast();
+        }
+    }
+
+    //判断是否是回文字符串
+    public static boolean checkPalindrome(char[] charArray,int left,int right){
+        while(left < right){
+            if(charArray[left] != charArray[right]){
+                return false;
+            }
+            right--;
+            left++;
+        }
+        return true;
+    }
+
+}
