5ccc7ee00b5822ebe708c0782c6be0426a44dc8b
luoyuntian <1140788816@qq.com>
2021-03-10 11:39:27 +0800

    update basic_calculator_224

--- src/main/java/basic_calculator_224/SolutionByAlex.java
+++ src/main/java/basic_calculator_224/SolutionByAlex.java
@@ -5,48 +5,71 @@ import java.util.Deque;
 
 public class SolutionByAlex {
     public static int calculate(String s) {
-        /*
-        创建一个栈，将字符依次压入栈中，如果遇到右括号，则弹出字符，直到弹出第一个左括号
-        将这些弹出的数据计算出结果，再次压入栈中，直到字符都已入栈
-        则将栈内元素弹出计算结果
-        不能使用字符分割，可能一个数字占用了多个字符
-         */
-        int length = s.length();
-        Deque<Character> stack = new ArrayDeque<>();
-        Deque<Character> tempStack = new ArrayDeque<>();
-        stack.push(s.charAt(0));
-        for(int i=1;i<length;i++){
-            char temp = s.charAt(i);
-            if(temp == ')'){
-                while(!stack.peek().equals('(')){
-                    tempStack.push(stack.pop());
+        Deque<Integer> nums = new ArrayDeque<>();
+        //处理第一个数为负数的情况
+        nums.addLast(0);
+
+        Deque<Character> ops = new ArrayDeque<>();
+
+        int n = s.length();
+        char[] cs = s.toCharArray();
+
+        for(int i=0;i<n;i++){
+            char c = cs[i];
+            if(c == ' '){
+                continue;
+            }else if(c == '('){
+                ops.addLast(c);
+            }else if(c == ')'){
+                //计算到最近一个右括号
+                while(!ops.isEmpty()){
+                    char op = ops.peekLast();
+                    if(op != '('){
+                        cal(nums,ops);
+                    }else {
+                        ops.pollLast();
+                        break;
+                    }
                 }
-                Character reuslt = getResult(tempStack);
-                stack.push(reuslt);
-                System.out.println("reuslt "+reuslt);
-                tempStack.clear();
             }else {
-                stack.push(temp);
+                if(isNum(c)){
+                    int u =0;
+                    int j =i;
+                    //从i开始连续数字取出，加入nums
+                    while(j < n && isNum(cs[j])){
+                        u = u* 10 +(int)(cs[j++] -'0');
+                        nums.addLast(u);
+                        i = j-1;
+                    }
+                }else {
+                    while(!ops.isEmpty() && ops.peekLast() != ')'){
+                        cal(nums,ops);
+                    }
+                    ops.addLast(c);
+                }
             }
         }
-
-        return Integer.valueOf(getResult(stack));
+        while (!ops.isEmpty()){
+            cal(nums,ops);
+        }
+        return nums.peekLast();
     }
 
 
-    public static char getResult(Deque<Character> stack){
-        int result =Integer.valueOf(stack.pop());
-        while(!stack.isEmpty()){
-            Character c = stack.pop();
-            if(c.equals('+')){
-                result += Integer.valueOf(stack.pop());
-            }
-            if(c.equals('-')){
-                result -= Integer.valueOf(stack.pop());
-            }
-        }
+    public static boolean isNum(char c){
+        return Character.isDigit(c);
+    }
 
-        return (char)result;
+    public static void cal(Deque<Integer> nums,Deque<Character> ops){
+        if(nums.isEmpty() || nums.size() < 2){
+            return;
+        }
+        if(ops.isEmpty()){
+            return;
+        }
+        int b = nums.pollLast(),a=nums.pollLast();
+        char op = ops.pollLast();
+        nums.addLast(op == '+' ? a+b : a-b);
     }
 
 
