cdb372564fd7b6fa79517d36ec2a430f8310e6e0
luoyuntian <1140788816@qq.com>
2021-03-16 11:16:32 +0800

    create reorder_list_143

--- src/main/java/reorder_list_143/SolutionByAlex.java
+++ src/main/java/reorder_list_143/SolutionByAlex.java
@@ -5,6 +5,18 @@ import common.ListNode;
 
 public class SolutionByAlex {
 
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+     * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+     */
     public void reorderList(ListNode head) {
 
     }
