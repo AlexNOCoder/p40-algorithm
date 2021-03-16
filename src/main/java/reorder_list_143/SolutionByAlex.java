69c8c1a42f6a50ce0f65465c74af94f697bd014d
luoyuntian <1140788816@qq.com>
2021-03-16 14:17:13 +0800

    update reorder_list_143

--- src/main/java/reorder_list_143/SolutionByAlex.java
+++ src/main/java/reorder_list_143/SolutionByAlex.java
@@ -3,6 +3,9 @@ package reorder_list_143;
 
 import common.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SolutionByAlex {
 
     /**
@@ -18,6 +21,23 @@ public class SolutionByAlex {
      * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
      */
     public void reorderList(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        while(head != null){
+            list.add(head);
+            head = head.next;
+        }
+        int n = list.size();
+        int i=0,j=n-1;
+        while (i < j){
+            list.get(i).next = list.get(j);
+            i++;
+            if(i == j){
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
 
     }
 }
