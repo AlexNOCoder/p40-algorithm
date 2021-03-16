04a81d50c70b363615a569ad6bc21f4678ec8b6a
luoyuntian <1140788816@qq.com>
2021-03-16 09:38:28 +0800

    update basic_calculator_224

--- src/main/java/basic_calculator_224/SolutionByAlex.java
+++ src/main/java/basic_calculator_224/SolutionByAlex.java
@@ -2,6 +2,7 @@ package basic_calculator_224;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 public class SolutionByAlex {
 //    代码里面:
@@ -87,10 +88,42 @@ public class SolutionByAlex {
         nums.addLast(op == '+' ? a+b : a-b);
     }
 
+    public static int cal2(String s){
+        Deque<Integer> ops = new LinkedList<>();
+        ops.push(1);
+        int sign = 1;
+        int res = 0;
+        int n = s.length();
+        int i =0;
+        while(i < n){
+            if(s.charAt(i) == ' '){
+                i++;
+            }else if(s.charAt(i) == '+'){
+                sign = ops.peek();
+                i++;
+            }else if(s.charAt(i) == '-'){
+                sign = -ops.peek();
+                i++;
+            }else if(s.charAt(i) == '('){
+                ops.pop();
+                i++;
+            }else {
+                long num = 0;
+                while(i < n && Character.isDigit(s.charAt(i))){
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                res += sign * num;
+            }
+        }
+
+        return res;
+    }
+
 
     public static void main(String[] args) {
-        String input = "(1+2)-(2-3)";
-        System.out.println(calculate(input));
+        String input = "(1+(4+5+2)-3)+(6+8)";
+        System.out.println(cal2(input));
     }
 
 }
