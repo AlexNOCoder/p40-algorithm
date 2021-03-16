1735f89c33967e83ec3695a3a71e00d6b55155db
luoyuntian <1140788816@qq.com>
2021-03-16 15:47:09 +0800

    update permutations_46

--- src/main/java/permutations_46/SolutionByAlex.java
+++ src/main/java/permutations_46/SolutionByAlex.java
@@ -1,11 +1,12 @@
 package permutations_46;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class SolutionByAlex {
-    public List<List<Integer>> permute(int[] nums) {
+    public static List<List<Integer>> permute(int[] nums) {
         //回溯法：一种通过探索所有可能的候选解来找出所有的解的算法。
         // 如果候选解被确认不是一个解（或者至少不是最后一个解），回溯算法会通过在上一步进行一些变化抛弃该解，即回溯并且再次尝试。
         List<List<Integer>> res = new ArrayList<>();
@@ -20,20 +21,24 @@ public class SolutionByAlex {
         return res;
     }
 
-    public void backtrack(int n,List<Integer> output,List<List<Integer>> res,int first){
+    public static void backtrack(int n,List<Integer> output,List<List<Integer>> res,int first){
         //结束条件，所有数字都已经选完
         if(first == n){
+            System.out.println("put: "+output.toString());
             res.add(new ArrayList<>(output));
         }
-
         for(int i = first;i<n;i++){
             //维护动态数组
             Collections.swap(output,first,i);
-
             //继续填下一个数
             backtrack(n,output,res,first+1);
             //撤销
             Collections.swap(output,first,i);
         }
+    }
+
+    public static void main(String[] args) {
+        int [] input = {1,2,3};
+        permute(input);
     }
 }
