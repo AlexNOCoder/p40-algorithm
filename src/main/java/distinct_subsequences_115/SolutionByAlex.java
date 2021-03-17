a1050963fc92be0af9d8e0b67824557bad95dd68
luoyuntian <1140788816@qq.com>
2021-03-17 10:49:34 +0800

    update distinct_subsequences_115

--- src/main/java/distinct_subsequences_115/SolutionByAlex.java
+++ src/main/java/distinct_subsequences_115/SolutionByAlex.java
@@ -16,23 +16,26 @@ public class SolutionByAlex {
          */
         int m = s.length();
         int n = t.length();
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(j == 0){
-                    dp[i][j] = 1;
-                }else if(i == 0){
-                    dp[i][j] = 0;
-                }else {
-                    if(s.charAt(i) == t.charAt(j)){
-                        dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                    }else {
-                        dp[i][j] = dp[i-1][j];
-                    }
+        if(m < n){
+         return 0;
+        }
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            char sChar = s.charAt(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tChar = t.charAt(j);
+                if (sChar == tChar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
                 }
             }
         }
-        return dp[m-1][n-1];
+        return dp[0][0];
     }
 
 }
