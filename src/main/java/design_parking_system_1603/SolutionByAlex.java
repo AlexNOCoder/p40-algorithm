52ada635247cd892dd9ac1c4063d68cf1758e489
luoyuntian <1140788816@qq.com>
2021-03-19 09:58:35 +0800

    create design_parking_system_1603

---
+++ src/main/java/design_parking_system_1603/SolutionByAlex.java
@@ -0,0 +1,18 @@
+package design_parking_system_1603;
+
+public class SolutionByAlex {
+
+    public SolutionByAlex(int big, int medium, int small) {
+
+    }
+
+    public boolean addCar(int carType) {
+        return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
