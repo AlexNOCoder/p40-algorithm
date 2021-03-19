a730a03a9d7d4045fb2cccb29a077aaf4c5eff76
luoyuntian <1140788816@qq.com>
2021-03-19 11:22:20 +0800

    update design_parking_system_1603

--- src/main/java/design_parking_system_1603/SolutionByAlex.java
+++ src/main/java/design_parking_system_1603/SolutionByAlex.java
@@ -3,7 +3,8 @@ package design_parking_system_1603;
 public class SolutionByAlex {
 
     public SolutionByAlex(int big, int medium, int small) {
-        //test
+        //test111
+
     }
 
     public boolean addCar(int carType) {
