3c89b4acaabbf8ce232e0731fc5e6bd8f85a1162
luoyuntian <1140788816@qq.com>
2021-03-23 09:49:16 +0800

    create flatten_nested_list_iterator_341

---
+++ src/main/java/flatten_nested_list_iterator_341/SolutionByAlex.java
@@ -0,0 +1,24 @@
+package flatten_nested_list_iterator_341;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class SolutionByAlex implements Iterator<Integer> {
+
+    public SolutionByAlex(List<SolutionByAlex> nestedList) {
+
+    }
+
+    @Override
+    public Integer next() {
+
+        return 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+}
+
+
