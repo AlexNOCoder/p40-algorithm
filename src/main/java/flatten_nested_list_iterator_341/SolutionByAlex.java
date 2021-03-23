2231e36fccfa559875f464bc8d3b3e5c648f4903
luoyuntian <1140788816@qq.com>
2021-03-23 11:03:24 +0800

    update flatten_nested_list_iterator_341

--- src/main/java/flatten_nested_list_iterator_341/SolutionByAlex.java
+++ src/main/java/flatten_nested_list_iterator_341/SolutionByAlex.java
@@ -1,23 +1,41 @@
 package flatten_nested_list_iterator_341;
 
+import common.NestedInteger;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class SolutionByAlex implements Iterator<Integer> {
 
-    public SolutionByAlex(List<SolutionByAlex> nestedList) {
+    private List<Integer> vals;
+    private Iterator<Integer> cur;
 
+    public SolutionByAlex(List<NestedInteger> nestedList) {
+        vals = new ArrayList<>();
+        dfs(nestedList);
+        cur = vals.iterator();
     }
 
     @Override
     public Integer next() {
 
-        return 0;
+        return cur.next();
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return cur.hasNext();
+    }
+
+    public void dfs(List<NestedInteger> nestedIntegers){
+        for(NestedInteger nest:nestedIntegers){
+            if(nest.isInteger()){
+                vals.add(nest.getInteger());
+            }else {
+                dfs(nest.getList());
+            }
+        }
     }
 }
 
