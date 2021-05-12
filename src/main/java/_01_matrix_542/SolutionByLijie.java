328f7b9aa8d19536ea54ffb61ca3e46d6b3f7549
lijie <lijie@cechealth.cn>
2021-05-12 11:11:34 +0800

    test

---
+++ src/main/java/_01_matrix_542/SolutionByLijie.java
@@ -0,0 +1,13 @@
+package _01_matrix_542;
+
+/**
+ * @version 1.0
+ * @author： lijie
+ * @date： 2021-05-12 11:10
+ */
+public class SolutionByLijie {
+
+    public void list(){
+        return;
+    }
+}
