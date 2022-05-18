c753297bb08bbb5ca768bd0c83e1970d59c2158b
luoyuntian <1140788816@qq.com>
2022-05-18 17:29:00 +0800

    add binary_search_704

---
+++ src/main/java/binary_search_704/SolutionByAlex.java
@@ -0,0 +1,23 @@
+package binary_search_704;
+
+public class SolutionByAlex {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+}
