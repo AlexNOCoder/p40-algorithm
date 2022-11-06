b86d3cc74fc193914891cacea9b5c92296f5a2a5
李杰 <lijie@cechealth.cn>
2022-11-06 22:32:55 +0800

    数组主题：1.数组的初始化。

---
+++ src/main/java/zzelements/arr/Array.java
@@ -0,0 +1,17 @@
+package zzelements.arr;
+
+/**
+ * @program: p40-algorithm
+ * @description: 数组初始化
+ * @author: lijie
+ * @create: 2022-11-06 22:27
+ */
+public class Array {
+    /**
+     * 数组的题目主要考察对代码的掌控能力
+     *数组是存放在连续空间上相同类型的集合。
+     */
+    public static void tastArr(){
+        int  array[]= {0,1,2};
+    }
+}
