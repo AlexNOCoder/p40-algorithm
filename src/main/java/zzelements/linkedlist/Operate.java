6edccbbe19a35f557ec0f0663a358844ae2cb276
李杰 <lijie@cechealth.cn>
2022-11-14 23:50:58 +0800

    LikedList主题：38：get(index)/addAtHead(val)/addAtTail(val)/addAtIndex(index,val)/deleteAtIndex(index) 707

--- src/main/java/zzelements/linkedlist/Operate.java
+++ src/main/java/zzelements/linkedlist/Operate.java
@@ -27,4 +27,59 @@ public class Operate {
         }
         return dummyHead.next;
     }
+
+    static Integer size;
+    static ListNode dummyNode;
+    public static Integer get(int index){
+        if (index < 0 || index >= size){
+            return -1;
+        }
+        ListNode currentNode = dummyNode;
+        //包含一个虚拟节点，所有查找第index+1个节点
+        for (int i = 0; i <= index; i ++){
+            currentNode = currentNode.next;
+        }
+        return currentNode.val;
+    }
+
+    public static void addAtHead(int val){
+        addAtHead(0, val);
+    }
+
+    public static void addAtTail(int val){
+        addAtHead(size, val);
+    }
+
+    public static void addAtHead(int index, int val){
+        if (index > size){return;}
+        if (index < 0){
+            index = 0;
+        }
+        size ++;
+        //找到要插入节点的前驱
+        ListNode pred = dummyNode;
+        for (int i = 0; i < index; i ++){
+            pred = pred.next;
+        }
+        ListNode toAdd = new ListNode(val);
+        toAdd.next = pred.next;
+        pred.next = toAdd;
+    }
+
+    //删除第index个节点
+    public static void deleteAtIndex(int index){
+        if (index < 0 || index >= size){
+            return;
+        }
+        size--;
+        if (index == 0){
+            dummyNode = dummyNode.next;
+            return;
+        }
+        ListNode pred = dummyNode;
+        for (int i = 0; i < index; i ++){
+            pred = pred.next;
+        }
+        pred.next = pred.next.next;
+    }
 }
